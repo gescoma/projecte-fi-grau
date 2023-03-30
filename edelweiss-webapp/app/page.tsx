d3681dbefd22e95afc6c74a26c1f57b6cd8067e7
joseAbaldea <jramon8888@gmail.com>
2023-03-30 21:43:59 +0200

    Cambio pagina pral

--- edelweiss-webapp/app/page.tsx
+++ edelweiss-webapp/app/page.tsx
@@ -2,36 +2,25 @@ import { Card } from '@/components/card'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from './page.module.css'
+import { Button } from '@/components/button'
+import { Avatar } from '@/components/avatar'
+import { Input } from '@/components/input'
+import { Logo } from '@/components/logo'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <div className={styles.main}>
-      <Card >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam incidunt porro minus tenetur aperiam ipsa nostrum corporis eaque autem minima enim iure, quaerat, non unde cum quo ab ipsum dolore.
-        Commodi repudiandae consectetur illum, minima odit praesentium ad eos consequuntur. Nesciunt quisquam reprehenderit quam corrupti? Totam tempore aspernatur modi cumque aperiam dolorum reiciendis sunt corporis aliquam, provident consequuntur, quia debitis.
-        Earum amet molestiae repellendus est hic libero beatae necessitatibus veniam quod officiis inventore nam reprehenderit, eius quam exercitationem praesentium expedita quidem dolorem delectus sed commodi, excepturi debitis? Dolores, culpa corrupti.
-        Harum quas nam ut possimus qui. Natus atque ipsam incidunt exercitationem voluptate, minima veritatis rem accusamus blanditiis, dicta odit quam, asperiores quia ullam corrupti harum dolorum tempora et deserunt minus?
-        Accusamus ea quod aperiam eveniet beatae illo quos laboriosam modi a dolore officiis minus, doloribus sapiente distinctio soluta officia odio eum! Eius unde dolorum omnis tempore ullam totam harum quos.
-        Voluptatum, earum officiis maiores eligendi quaerat commodi recusandae dolores nam eveniet nesciunt sunt exercitationem cupiditate maxime officia ut praesentium. Recusandae, porro possimus exercitationem vero pariatur iure nemo enim deserunt maxime!
-        Fuga dignissimos dolore voluptatum nisi omnis aspernatur inventore excepturi, voluptates, voluptatem minima rerum magnam. Facilis eaque fuga obcaecati excepturi quas quae ducimus dolore nulla recusandae, expedita, deserunt consequuntur tenetur maiores.
-        Veritatis repellat tenetur deleniti modi, commodi consequuntur excepturi! Dolores dignissimos ipsa quia praesentium sunt quisquam ipsum omnis tempore in illo, iure nulla, rem ut. Exercitationem iste ab pariatur deleniti quam.
-        Odio, ullam, laudantium omnis, saepe nostrum minima aspernatur eligendi maxime et ratione sunt ipsa voluptates. Accusantium, dolorem incidunt, consectetur quam magnam optio, blanditiis beatae voluptatibus veniam culpa vitae mollitia rem.
-        Accusamus consectetur harum voluptatum facere tenetur sapiente incidunt, omnis doloribus, dignissimos illum voluptates sed veritatis inventore nostrum. Quasi, laboriosam incidunt aut commodi fugit assumenda maiores, omnis facere hic similique eveniet?
+      <Card > 
+      <Logo></Logo>
+      <Input>Login</Input>
+      <Input>Passwd</Input>     
+      <Button isBoton>Enviar</Button> 
+      <Avatar>2</Avatar>    
       </Card>
-      <Card >
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam incidunt porro minus tenetur aperiam ipsa nostrum corporis eaque autem minima enim iure, quaerat, non unde cum quo ab ipsum dolore.
-        Commodi repudiandae consectetur illum, minima odit praesentium ad eos consequuntur. Nesciunt quisquam reprehenderit quam corrupti? Totam tempore aspernatur modi cumque aperiam dolorum reiciendis sunt corporis aliquam, provident consequuntur, quia debitis.
-        Earum amet molestiae repellendus est hic libero beatae necessitatibus veniam quod officiis inventore nam reprehenderit, eius quam exercitationem praesentium expedita quidem dolorem delectus sed commodi, excepturi debitis? Dolores, culpa corrupti.
-        Harum quas nam ut possimus qui. Natus atque ipsam incidunt exercitationem voluptate, minima veritatis rem accusamus blanditiis, dicta odit quam, asperiores quia ullam corrupti harum dolorum tempora et deserunt minus?
-        Accusamus ea quod aperiam eveniet beatae illo quos laboriosam modi a dolore officiis minus, doloribus sapiente distinctio soluta officia odio eum! Eius unde dolorum omnis tempore ullam totam harum quos.
-        Voluptatum, earum officiis maiores eligendi quaerat commodi recusandae dolores nam eveniet nesciunt sunt exercitationem cupiditate maxime officia ut praesentium. Recusandae, porro possimus exercitationem vero pariatur iure nemo enim deserunt maxime!
-        Fuga dignissimos dolore voluptatum nisi omnis aspernatur inventore excepturi, voluptates, voluptatem minima rerum magnam. Facilis eaque fuga obcaecati excepturi quas quae ducimus dolore nulla recusandae, expedita, deserunt consequuntur tenetur maiores.
-        Veritatis repellat tenetur deleniti modi, commodi consequuntur excepturi! Dolores dignissimos ipsa quia praesentium sunt quisquam ipsum omnis tempore in illo, iure nulla, rem ut. Exercitationem iste ab pariatur deleniti quam.
-        Odio, ullam, laudantium omnis, saepe nostrum minima aspernatur eligendi maxime et ratione sunt ipsa voluptates. Accusantium, dolorem incidunt, consectetur quam magnam optio, blanditiis beatae voluptatibus veniam culpa vitae mollitia rem.
-        Accusamus consectetur harum voluptatum facere tenetur sapiente incidunt, omnis doloribus, dignissimos illum voluptates sed veritatis inventore nostrum. Quasi, laboriosam incidunt aut commodi fugit assumenda maiores, omnis facere hic similique eveniet?
-      </Card>
+      
+
     </div>
   )
 }
