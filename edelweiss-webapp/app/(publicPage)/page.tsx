95b8659516f86b354163aecfed4981a2b87e1c59
Gerard Escolies <gerard.esma@icloud.com>
2023-04-04 19:53:20 +0200

    changes

---
+++ edelweiss-webapp/app/(publicPage)/page.tsx
@@ -0,0 +1,18 @@
+import { Avatar } from "@/components/avatar"
+import { Button } from "@/components/button"
+import { Card } from "@/components/card"
+import { Input } from "@/components/input"
+import { Logo } from "@/components/logo"
+import styles from "./page.module.css"
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+      <Card>
+        <Logo width={300} height={150} />
+        <Button isBoton>Enviar</Button>
+        <Avatar>2</Avatar>
+      </Card>
+    </div>
+  )
+}
