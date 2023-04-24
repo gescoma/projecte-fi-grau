90e0a799eea9143b7ac5f5ffd22f3a1e4863d20d
Gerard Escolies <gerard.esma@icloud.com>
2023-04-24 19:30:43 +0000

    update schemas

--- edelweiss_server/src/schemas.py
+++ edelweiss_server/src/schemas.py
@@ -15,3 +15,29 @@ class Campain(BaseModel):
         """pydantic to create types for tables"""
         orm_mode = True
     # cliente: Client
+
+
+class UserDB(BaseModel):
+    """user db model"""
+    name: str
+    image: str
+    password: str
+    role: str
+    email: str
+    uid: str
+    client_id: str
+
+
+class UserData(BaseModel):
+    """user data model"""
+    email: str
+    password: str
+    csrfToken: str
+    callbackUrl: str
+    bearer: str
+
+
+class UserJWT(BaseModel):
+    """user jwt model"""
+    user_id: str
+    user_role: str
