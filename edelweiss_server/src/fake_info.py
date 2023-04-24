ed4d9d4fb53069a7e0166bb847e0d2da65e4514d
Gerard Escolies <gerard.esma@icloud.com>
2023-04-24 19:31:31 +0000

    refractor fakeinfo

---
+++ edelweiss_server/src/fake_info.py
@@ -0,0 +1,19 @@
+"""Fake info for testing purposes"""
+from src.schemas import UserDB
+
+fake_db_user: list[UserDB] = [{
+    "name": "Gerard Escolies",
+    "image": "https://unavatar.io/gescoma",
+    "password": "password",
+    "role": "admin",
+    "email": "gerard@edelweiss.com",
+    "uid": "ff5d87a9-6ada-4248-9901-14d2d983b958"
+},
+    {
+    "name": "Example user",
+    "image": "https://unavatar.io/example",
+    "password": "password",
+    "role": "user",
+    "email": "user@edelweiss.com",
+    "uid": "ff5d87a9-6ada-4248-9901-14d2d983b958"
+}]
