2263cd213a68aa43ef14d57eb2d113eaacb917e9
Gerard Escolies <gerard.esma@icloud.com>
2023-04-24 19:31:43 +0000

    crud modifications

--- edelweiss_server/src/user/crud.py
+++ edelweiss_server/src/user/crud.py
@@ -1,9 +1,9 @@
 """User CRUD operations."""
-from . import fake_info
+from src.fake_info import fake_db_user
 
 
 def get_user(email):
-    for user in fake_info.fake_db_user:
+    for user in fake_db_user:
         if user["email"] == email:
             return user
     return None
