cb7e29f2c3f961b081186e80232091f69467a295
Gerard Escolies <gerard.esma@icloud.com>
2023-05-01 12:46:37 +0000

    add selectionbox component

---
+++ edelweiss_webapp/components/filters/selectionBox/index.tsx
@@ -0,0 +1,18 @@
+import { forwardRef, useEffect, useRef } from "react"
+
+function Checkbox({ indeterminate, ...rest }: any, ref: any) {
+  const defaultRef = useRef()
+  const resolvedRef = ref || defaultRef
+
+  useEffect(() => {
+    resolvedRef.current.indeterminate = indeterminate
+  }, [resolvedRef, indeterminate])
+
+  return (
+    <>
+      <input type="checkbox" ref={resolvedRef} {...rest} />
+    </>
+  )
+}
+
+export const IndeterminateCheckbox = forwardRef(Checkbox)
