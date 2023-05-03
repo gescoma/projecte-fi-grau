d7fa66025f4791614f90023e5737bb1a51dcc5f2
Gerard Escolies <gerard.esma@icloud.com>
2023-05-03 15:54:57 +0000

    modifications on select box to make it work again

--- edelweiss_webapp/components/filters/dropdownBox/index.tsx
+++ edelweiss_webapp/components/filters/dropdownBox/index.tsx
@@ -1,11 +1,26 @@
+import { Dispatch, SetStateAction } from "react"
+
 import { Datatype } from "@/types/filterDataType"
 import styles from "./dropdownBox.module.css"
 
-export function DropdownFilter({ data, ...restOfProps }: { data: Datatype[] }) {
+export function DropdownBox({
+  data,
+  action,
+  state,
+  ...restOfProps
+}: {
+  data: Datatype[]
+  action: Dispatch<SetStateAction<string>>
+  state: any
+}) {
   return (
-    <select {...restOfProps}>
+    <select {...restOfProps} onChange={(e) => action(e.target.value)}>
       {data.map(({ value, label, items }) => (
-        <option key={label.replace(" ", "")} value={value}>
+        <option
+          key={label.replace(" ", "")}
+          value={value}
+          selected={value === state}
+        >
           {label}
           {items && <span>{items}</span>}
         </option>
