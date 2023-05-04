a2421cdabedee056cee894a5ef7048d9ae6a17f3
Gerard Escolies <gerard.esma@icloud.com>
2023-05-04 22:00:16 +0000

    properties use case for table and grid layouts

---
+++ edelweiss_webapp/components/properties/sidebar.tsx
@@ -0,0 +1,28 @@
+export function Sidebar({ data }: { data: any }) {
+  return (
+    <>
+      <h1>Vivienda</h1>
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      {JSON.stringify(data, null, "  ")}
+      <var>hello</var>
+    </>
+  )
+}
