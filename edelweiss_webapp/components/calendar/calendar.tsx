52b7b67b0ce23bc064a2d4ef4d9f0743efb60a7c
Gerard Escolies <gerard.esma@icloud.com>
2023-05-10 19:56:30 +0200

    create calendar component

--- edelweiss_webapp/components/calendar/calendar.tsx
+++ edelweiss_webapp/components/calendar/calendar.tsx
@@ -1,24 +1,47 @@
-import "./calendar.css"
+import "react-big-calendar/lib/css/react-big-calendar.css"
 
-import { Calendar as ReactCalendar, dayjsLocalizer } from "react-big-calendar"
+import { Calendar as BigCalendar, dayjsLocalizer } from "react-big-calendar"
+import { ComponentType, ReactNode, useState } from "react"
 
 import { EventModel } from "@/utils/calendar/models"
+import { SidebarLayout } from "../sidebar/sidebar"
 import dayjs from "dayjs"
 import timezone from "dayjs/plugin/timezone"
-import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop"
-
-const DnDCalendar = withDragAndDrop(ReactCalendar)
 
 dayjs.extend(timezone)
 const localizer = dayjsLocalizer(dayjs)
 
-export function Calendar({ events }: { events: EventModel[] }) {
+export function Calendar({
+  events,
+  sidebar: Sidebar,
+}: {
+  events: EventModel[]
+  sidebar: ComponentType<any>
+}) {
+  const [sidebarOpen, setSidebarOpen] = useState(false)
+  const [sidebarTask, setSidebarTask] = useState<EventModel | null>(null)
   return (
-    <DnDCalendar
-      localizer={localizer}
-      onDoubleClickEvent={(event) => {
-        console.log(event)
-      }}
-    />
+    <>
+      <BigCalendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 500 }}
+        onDoubleClickEvent={(e) => {
+          setSidebarOpen(true)
+          setSidebarTask(e)
+        }}
+      />
+      {sidebarOpen && (
+        <SidebarLayout
+          closeAction={() => {
+            setSidebarOpen(false)
+          }}
+        >
+          <Sidebar task={sidebarTask} />
+        </SidebarLayout>
+      )}
+    </>
   )
 }
