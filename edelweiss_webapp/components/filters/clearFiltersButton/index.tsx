b024b6a84fdf8c43f261df1121d0502bd9775d84
Gerard Escolies <gerard.esma@icloud.com>
2023-05-11 20:42:06 +0200

    stylize clean filters button

--- edelweiss_webapp/components/filters/clearFiltersButton/index.tsx
+++ edelweiss_webapp/components/filters/clearFiltersButton/index.tsx
@@ -1,3 +1,24 @@
+import { FiFilter, FiTrash2, FiXCircle } from "react-icons/fi"
+
+import styles from "./clearFiltersButton.module.css"
+
 export function ClearFilters({ ...props }) {
-  return <button {...props}>Limpiar</button>
+  const { className, disabled, ...restOfProps } = props
+  return (
+    <div className={styles.container}>
+      <button
+        className={`${styles.button} ${className && className} ${
+          !disabled && styles.active
+        }`}
+        disabled={disabled}
+        {...restOfProps}
+      >
+        Limpiar filtros
+        <div className={styles.iconGroup}>
+          <FiFilter className={styles.icon} />
+          <FiXCircle className={styles.iconSmall} />
+        </div>
+      </button>
+    </div>
+  )
 }
