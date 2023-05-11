2262feb31fa9152b47ac43e44608a285cd75f4ab
Gerard Escolies <gerard.esma@icloud.com>
2023-05-11 19:58:26 +0200

    estilizar el select

--- edelweiss_webapp/components/filters/dropdownBox/index.tsx
+++ edelweiss_webapp/components/filters/dropdownBox/index.tsx
@@ -1,6 +1,8 @@
 import { Dispatch, SetStateAction, useState } from "react"
 
 import { Datatype } from "@/types/filterDataType"
+import { FiChevronDown } from "react-icons/fi"
+import { IconType } from "react-icons"
 import styles from "./dropdown.module.css"
 
 export function DropdownBox({
@@ -13,21 +15,52 @@ export function DropdownBox({
   action: Dispatch<SetStateAction<string>>
   state: any
 }) {
-  const [selected, setSelected] = useState(state)
+  const [selected, setSelected] = useState<string | IconType>(
+    data.filter((item) => item.value === state)[0].label as string | IconType
+  )
   const [selectOpen, setSelectOpen] = useState(false)
+
+  console.log({ state })
+
+  const handleClick = (value: string, label: string | IconType) => {
+    setSelected(label)
+    action(value)
+    setSelectOpen(false)
+  }
+
+  console.log({ selected, selectOpen })
   return (
-    <div className={styles.select}>
-      {selected}
-      {selectOpen && (
-        <div className={styles.options}>
-          {data.map(({ value, label, items }) => (
-            <div key={value}>
-              {typeof label === "string" ? label : label({})}
-              {/* {items && <span>{items}</span>} */}
-            </div>
-          ))}
+    <>
+      <div className={styles.select}>
+        <span>Owners:</span>
+        <div
+          className={`${styles.input} ${selectOpen && styles.open}`}
+          onClick={() => setSelectOpen(!selectOpen)}
+        >
+          <div className={styles.label}>
+            {typeof selected === "string" ? selected : selected({})}
+            <FiChevronDown
+              className={`${styles.caret} ${selectOpen && styles.turnCaret}`}
+            />
+          </div>
         </div>
-      )}
-    </div>
+        {selectOpen && (
+          <div className={styles.optionsContainer}>
+            <div className={styles.options}>
+              {data.map(({ value, label, items }) => (
+                <div
+                  className={styles.value}
+                  key={value}
+                  onClick={() => handleClick(value, label)}
+                >
+                  {typeof label === "string" ? label : label({})}
+                  {/* {items && <span>{items}</span>} */}
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
+    </>
   )
 }
