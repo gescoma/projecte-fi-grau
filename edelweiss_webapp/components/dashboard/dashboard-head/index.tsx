a0b5f08b67556a6d74abbe7df9880c9c29e86c7b
Gerard Escolies <gerard.esma@icloud.com>
2023-05-11 23:17:08 +0200

    dashboard head modification

--- edelweiss_webapp/components/dashboard/dashboard-head/index.tsx
+++ edelweiss_webapp/components/dashboard/dashboard-head/index.tsx
@@ -1,8 +1,13 @@
+import { SearchBox } from "@/components/input/searchBox"
+import styles from "./head.module.css"
+import { useState } from "react"
+
 export function DashboardHead({ children }: { children: string }) {
+  const [search, setSearch] = useState("")
   return (
-    <div>
+    <div className={styles.head}>
       <h1>{children}</h1>
-      <input type="text" />
+      <SearchBox onChange={setSearch} value={search} />
     </div>
   )
 }
