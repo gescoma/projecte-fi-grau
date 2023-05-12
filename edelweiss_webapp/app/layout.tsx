2c78ed03a5c7cd65f33863be5c864b6a3ee4b776
Gerard Escolies <gerard.esma@icloud.com>
2023-05-12 14:39:30 +0200

    Update layout.tsx

--- edelweiss_webapp/app/layout.tsx
+++ edelweiss_webapp/app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
-        <SupabaseProvider className={inter.className} session={session}>{children}</SupabaseProvider>
+      <body className={inter.className}>
+        <SupabaseProvider session={session}>{children}</SupabaseProvider>
       </body>
     </html>
   )
