ab7af6d998719a28473cb978d1e7b35f731a40b6
Gerard Escolies <gerard.esma@icloud.com>
2023-05-13 17:20:36 +0200

    change filters button name

--- edelweiss_webapp/components/filters/filtersButton/index.tsx
+++ edelweiss_webapp/components/filters/filtersButton/index.tsx
@@ -2,22 +2,24 @@ import { FiFilter, FiTrash2, FiXCircle } from "react-icons/fi"
 
 import styles from "./clearFiltersButton.module.css"
 
-export function ClearFilters({ ...props }) {
-  const { className, disabled, ...restOfProps } = props
+export function FiltersButton({
+  type = "normal",
+  ...props
+}: {
+  type?: string
+  disabled?: boolean
+} & React.HTMLAttributes<HTMLButtonElement>) {
+  const { className, disabled, children, ...restOfProps } = props
   return (
     <div className={styles.container}>
       <button
         className={`${styles.button} ${className && className} ${
           !disabled && styles.active
-        }`}
+        } ${styles[type]}`}
         disabled={disabled}
         {...restOfProps}
       >
-        Limpiar filtros
-        <div className={styles.iconGroup}>
-          <FiFilter className={styles.icon} />
-          <FiXCircle className={styles.iconSmall} />
-        </div>
+        {children}
       </button>
     </div>
   )
